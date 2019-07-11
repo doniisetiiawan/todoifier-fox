ad47246149c0f492f911db101ec0abd97a38d1d1
Doni <donisetiawan.050@gmail.com>
2019-07-11 17:23:05 +0700

    Todo index API request

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 4000;
+
+const todos = [
+  {
+    id: 1, description: 'Write some code', done: false, critical: false,
+  },
+  {
+    id: 2, description: 'Change the world', done: false, critical: false,
+  },
+  {
+    id: 3, description: 'Eat a cookie', done: false, critical: false,
+  },
+];
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.json({}));
+app.get('/api/todos', (req, res) => res.json({ todos }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
