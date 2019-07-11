de65ba8668841d7c3205c175a79b4ffc56fc9e9f
Doni <donisetiawan.050@gmail.com>
2019-07-11 15:28:16 +0700

    Refactor header

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,21 @@
 /* eslint-disable react/jsx-filename-extension,react/prop-types */
 import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import TodoList from '../TodoList';
+
 import './App.css';
 
-const details = {
-  header: 'Todoifier',
-  headerColor: 'red',
-};
+const headerDisplay = title => (
+  <Navbar color="dark" dark expand="md">
+    <NavbarBrand href="/">{title}</NavbarBrand>
+  </Navbar>
+);
 
-const moreDetails = {
-  ...details,
-  header: 'Best Todoifier',
-  background: 'black',
-};
-
-const headerDisplay = ({
-  header: title = 'Todo List',
-  headerColor: color = 'blue',
-  background: background = 'none',
-}) => <h2 style={{ color, background }}>{title}</h2>;
+const headerTitle = 'Todoifier';
 
 const App = () => (
   <div className="App">
-    {headerDisplay(moreDetails)}
+    {headerDisplay(headerTitle)}
     <br />
     <TodoList />
   </div>
